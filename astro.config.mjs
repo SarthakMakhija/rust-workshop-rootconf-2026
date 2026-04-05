ca7ffbe02f465a252cdbe6a73899ff6e54c4dc47
Sarthak Makhija <sarthak.makhija@gmail.com>
2026-04-05 19:26:44 +0530

    Sarthak | Adds favicon, social-media icon, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tech-lessons.in',
   base: '/rust-workshop-2026/',
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
