edef657829caf591fb85104cde2d7a2a4f759c47
Sarthak Makhija <sarthak.makhija@gmail.com>
2026-04-05 17:48:15 +0530

    Sarthak | Updates astro.config.mjs to have the baseUrl as tech-lessons.in

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tech-lessons.in',
+  base: '/rust-workshop-2026/',
   integrations: [react()],
 });
